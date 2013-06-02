0c9a0f94644abb941284128d908377e3785eb9e4
Jose V. Trigueros <j.v.trigueros@gmail.com>
2013-06-02 03:36:55 -0600

    Added callback to the kml2json because we're all async-y

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 var fs = require('fs')
    ,xml2js = require('xml2js')
-   ,inspect = require('eyes').inspector({maxLength: false})
+//   ,inspect = require('eyes').inspector({maxLength: false})
    ,traverse = require('traverse')
 
 var parser = new xml2js.Parser({
@@ -15,7 +15,7 @@ var parser = new xml2js.Parser({
     explicitArray: false
 })
 
-function kml2json(kmlString) {
+function kml2json(kmlString, callback) {
     parser.parseString(kmlString, function(err, result) {
 //        inspect(result)
         var routes = result.document.placemark
@@ -43,34 +43,14 @@ function kml2json(kmlString) {
                 }
             })
 
-            var rawDescription = traverse(route.description.table.tr).reduce( function(acc,node) {
-                if(this.key === 'td') {
-                    if(node[0].match(/.*(vehicle).*/i)) {
-                        acc.push({vehicle: node[1]})
-                    } else if(node[0].match(/.*(speed).*/i)) {
-                        acc.push({speed: node[1]})
-                    } else if(node[0].match(/.*(time).*/i)) {
-                        acc.push({timestamp: node[1]})
-                    } else if(node[0].match(/.*(stop).*/i)) {
-                        acc.push({destination: node[1]})
-                    }
-                    this.delete(true)
-                }
-                return acc
-            })
-
-//            rawDescription = rawDescription.filter(function(n){ return Object.keys(n).length !== 0 })
-//            rawDescription.forEach(function(item) {
-//                var key = Object.keys(item)[0]
-//                currentRoute.description[key] = item[key]
-//            })
             jsonRoute.push(currentRoute)
         })
-        console.log(jsonRoute)
+//        console.log(jsonRoute)
 //        fs.writeFile('test/allroutes.json',JSON.stringify(jsonRoute,null,2))
+        callback(JSON.stringify(jsonRoute,null,2))
     })
 }
 
 fs.readFile(__dirname + '/test/allroutes.kml', function(err, kmlString) {
-  kml2json(kmlString)
+  kml2json(kmlString, console.log)
 })
